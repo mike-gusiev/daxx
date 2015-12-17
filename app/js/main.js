17ec22304f2744df1e02c551c84893108e1bf21a
mike-gusiev <mike-gusiev@gmail.com>
2015-12-18 01:34:28 +0200

    sort functionality

--- app/js/main.js
+++ app/js/main.js
@@ -8,12 +8,9 @@
 
     var StatesView = Backbone.View.extend({
         initialize: function () {
-            _.bindAll(this, 'render', 'sortField');
-            this.collection = new States;
-            this.collection.fetch({
-                success: this.render
-            });
-            this.collection.on('sort', this.render, this);
+            _.bindAll(this, 'render', 'initCollection', 'initProperties', 'sortField');
+            this.initCollection();
+            this.initProperties();
         },
 
         template: _.template($('#statesTemplate').html()),
@@ -22,23 +19,63 @@
             'click th': 'sortField'
         },
 
-        sortField: function (elem) {
-            var index = elem.currentTarget.cellIndex;
-            this.collection.comparator = function (a, b) {
-                var field = 'name';
-                if (index === 0) {
-                    field = 'abbreviation';
-                }
-                if (a.get(field) > b.get(field)) {
-                    return 1;
-                }
-                if (a.get(field) < b.get(field)) {
-                    return -1;
-                }
-                if (a.get(field) === b.get(field)) {
-                    return 0;
-                }
+        initCollection: function () {
+            this.collection = new States;
+            this.collection.fetch({
+                success: this.render
+            });
+            this.collection.on('sort', this.render, this);
+        },
+
+        initProperties: function () {
+            this.sortData = {
+                a: 'asc',
+                b: 'asc'
             };
+        },
+
+        getFieldName: function (elem) {
+            var index = elem.currentTarget.cellIndex;
+            var field = 'name';
+            if (index === 0) {
+                field = 'abbreviation';
+            }
+            return field;
+        },
+
+        getSortType: function (elem) {
+            var index = elem.currentTarget.cellIndex;
+            var result = 'desc';
+            var target = 'a';
+            if (index === 1) {
+                target = 'b';
+            }
+            if (this.sortData[target] === 'desc') {
+                result = 'asc';
+            }
+            this.sortData[target] = result;
+            return result;
+        },
+
+        sortField: function (elem) {
+            var field = this.getFieldName(elem);
+            var sortType = this.getSortType(elem);
+            this.collection.comparator = function (a, b) {
+                var val1 = a.get(field);
+                var val2 = b.get(field);
+                var result = 0;
+
+                if (val1 > val2) {
+                    result = 1;
+                }
+                if (val1 < val2) {
+                    result = -1;
+                }
+                if (sortType === 'desc') {
+                    result = -result;
+                }
+                return result;
+            }.bind(this);
             this.collection.sort();
         },
 
