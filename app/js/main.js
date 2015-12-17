28e5b3f33a8945946acd9db23ed4746e16c008b8
Mike Gusiev <mike.gusiev@omnigon.com>
2015-12-17 14:27:12 +0200

    backbone template

--- app/js/main.js
+++ app/js/main.js
@@ -15,8 +15,12 @@
             });
         },
 
+        template: _.template($('#statesTemplate').html()),
+
         render: function () {
-            console.log(this.collection.toJSON());
+            $(this.el).html(this.template({
+                states: this.collection.toJSON()[0]
+            }));
         }
     });
 
